758bc2c364730aa89e2583512c96e02641abe2a2
Tanay-Patel <61372997+Tanay-Patel@users.noreply.github.com>
2021-06-04 11:52:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,34 +12,43 @@ inputBox.forEach(input => {
     const colorVal = input.value;
     const boxID = input.id;
 
+    console.log(input.id);
+
     //Updating Value of textfeild to color
     input.style.color = colorVal;
 
     //This code updates all the correct ID's colors
     const curElements = document.querySelectorAll(`.${boxID}`);
     curElements.forEach(elem => {
+        //console.log(elem);
         //Checking for 'background' special-case
         if(boxID != 'background'){
             elem.style.color = colorVal;
         }
     });
 
+    
+
     //This code updates visuals on input
     input.addEventListener('blur', () => {
         
         //This gets the IDs and updates their color
         const curElements = document.querySelectorAll(`.${boxID}`);
+        
         curElements.forEach(elem => {
-            if(boxID === 'background'){
+            
+            if(input.id === 'background'){
                 document.querySelector('body').style.backgroundColor = input.value;
-            }
-            else if(boxID === 'foreground'){
-                document.querySelector('body').style.color = input.value;
             }
             else{
                 elem.style.color = input.value;
             }
         });
+
+        //Checking for 'foreground' special-case
+        if(input.id === 'foreground'){
+            document.querySelector('body').style.color = input.value;
+        }
         //Updates text input color
         input.style.color = input.value;
     });
